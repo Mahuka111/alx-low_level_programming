e81ecf9c32481ed7fa20404b97e67ccf1fb75574
Mahuka111 <hamtel19@gmail.com>
2022-03-15 08:26:26 -0700

    putchar function to print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "_putchar.c"
 /**
  * main - Prints _putchar  
  * 
@@ -13,7 +14,7 @@ int main(void)
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
-	_putchar('r')
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
